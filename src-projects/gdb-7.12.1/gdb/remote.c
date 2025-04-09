9d2abfc66fa97cf97f7843672adbb30f90d3aac4
mesheets <16882600+mesheets@users.noreply.github.com>
2025-04-08 22:04:43 -0400

    Apply GDB patches from Debian for Hurd support (upstream backports)

--- src-projects/gdb-7.12.1/gdb/remote.c
+++ src-projects/gdb-7.12.1/gdb/remote.c
@@ -6511,7 +6511,6 @@ Packet: '%s'\n"),
 	  else if (strprefix (p, p1, "exec"))
 	    {
 	      ULONGEST ignored;
-	      char pathname[PATH_MAX];
 	      int pathlen;
 
 	      /* Determine the length of the execd pathname.  */
@@ -6520,11 +6519,12 @@ Packet: '%s'\n"),
 
 	      /* Save the pathname for event reporting and for
 		 the next run command.  */
+	      char *pathname = (char *) xmalloc (pathlen + 1);
 	      hex2bin (p1, (gdb_byte *) pathname, pathlen);
 	      pathname[pathlen] = '\0';
 
 	      /* This is freed during event handling.  */
-	      event->ws.value.execd_pathname = xstrdup (pathname);
+	      event->ws.value.execd_pathname = pathname;
 	      event->ws.kind = TARGET_WAITKIND_EXECD;
 
 	      /* Skip the registers included in this packet, since
